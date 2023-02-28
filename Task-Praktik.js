3651747b7301795b782b00d9f35537c7ec5466c9
RezaASiregar <rezasiregar98@gmail.com>
2023-03-01 03:06:44 +0700

    TASK PRAK

---
+++ Task-Praktik.js
@@ -0,0 +1,45 @@
+// Aritmatika
+let angka1 = 11 + 11 + 11
+console.log(angka1)
+let angka2 = 11 - 11 - 11
+console.log(angka2)
+let angka3 = 11 * 11 / 11
+console.log(angka3)
+let angka4 = 11 + 11 * 11
+console.log(angka4)
+let angka5 = 11 / 11 + 11
+console.log(angka5)
+
+// Perbandingan
+let perbandingan1 = 11 > 12
+console.log(perbandingan1)
+let perbandingan2 = 11 > 12
+console.log(perbandingan2)
+let perbandingan3 = 11 < 12
+console.log(perbandingan3)
+let perbandingan4 = 11 < 12
+console.log(perbandingan4)
+let perbandingan5 = 13 >= 14
+console.log(perbandingan5)
+let perbandingan6 = 13 >= 14
+console.log(perbandingan6)
+let perbandingan7 = 13 <= 14
+console.log(perbandingan7)
+let perbandingan8 = 13 <= 14
+console.log(perbandingan8)
+let perbandingan16 = 15 == 16
+console.log(perbandingan16)
+let perbandingan9 = 15 == 16
+console.log(perbandingan9)
+let perbandingan10 = 15 === 16
+console.log(perbandingan10)
+let perbandingan11 = 15 === 16
+console.log(perbandingan11)
+let perbandingan12 = 15 != 16
+console.log(perbandingan12)
+let perbandingan13 = 15 != 16
+console.log(perbandingan13)
+let perbandingan14 = 15 !== 16
+console.log(perbandingan14)
+let perbandingan15 = 15 !== 16
+console.log(perbandingan15)
